317d7ed64762b7ca571e790cb603f07cdf433c45
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-08-22 20:35:47 +0000

    change the comments

--- extract_precision.c
+++ extract_precision.c
@@ -15,40 +15,40 @@
 
 int extract_precision(const char *formatString, int *currentIndex, va_list argsList)
 {
-    int index = *currentIndex + 1; // Start checking after the '%'
-    //p=precision
+    int index = *currentIndex + 1; /* Start checking after the '%' */
+    /* p=precision */
     int p = -1;
 
-    // Check if precision is specified by a '.'
+    /* Check if precision is specified by a '.' */
     if (formatString[index] != '.')
-        return p; // Precision not specified, return -1
+        return p; /* Precision not specified, return -1 */
 
     p = 0;
 
-    // Loop through the format string to find the precision value
+    /* Loop through the format string to find the precision value */
     for (index += 1; formatString[index] != '\0'; index++)
     {
-        // Check if the character is a digit
+        /* Check if the character is a digit */
         if (is_digit(formatString[index]))
         {
-            p *= 10; // Multiply the existing precision by 10
-            p += formatString[index] - '0'; // Add the digit to the precision
+            p *= 10; /* Multiply the existing precision by 10 */
+            p += formatString[index] - '0'; /* Add the digit to the precision */
         }
-        // Check if precision is specified as an asterisk (*)
+        /* Check if precision is specified as an asterisk (*) */
         else if (formatString[index] == '*')
         {
-            index++; // Move past the asterisk
-            p = va_arg(argsList, int); // Get precision from arguments
-            break; // Exit the loop when precision is found
+            index++; /* Move past the asterisk */
+            p = va_arg(argsList, int); /* Get precision from arguments */
+            break; /* Exit the loop when precision is found */
         }
         else
         {
-            break; // Stop if a non-digit, non-asterisk character is encountered
+            break; /* Stop if a non-digit, non-asterisk character is encountered */
         }
     }
 
-    // Update the current index to point to the last processed character
+    /* Update the current index to point to the last processed character */
     *currentIndex = index - 1;
 
-    return p; // Return the calculated precision
+    return p; /* Return the calculated precision */
 }
--- extract_size.c
+++ extract_size.c
@@ -9,24 +9,24 @@
  */
 int extract_size(const char *formatSpecifier, int *currentIndex)
 {
-    int nextIndex = *currentIndex + 1;  // Move to the next character in the formatSpecifier string.
-    int argumentSize = 0;  // Initialize the argument size to zero.
+    int nextIndex = *currentIndex + 1;  /* Move to the next character in the formatSpecifier string. */
+    int argumentSize = 0;  /* Initialize the argument size to zero. */
 
-    // Check if the next character in the formatSpecifier is 'l' (long).
+    /* Check if the next character in the formatSpecifier is 'l' (long). */
     if (formatSpecifier[nextIndex] == 'l') {
-        argumentSize = SIZE_LONG;  // Set the argument size to SIZE_LONG.
+        argumentSize = SIZE_LONG;  /* Set the argument size to SIZE_LONG. */
     }
-    // Check if the next character in the formatSpecifier is 'h' (short).
+    /* Check if the next character in the formatSpecifier is 'h' (short). */
     else if (formatSpecifier[nextIndex] == 'h') {
-        argumentSize = SIZE_SHORT;  // Set the argument size to SIZE_SHORT.
+        argumentSize = SIZE_SHORT;  /* Set the argument size to SIZE_SHORT. */
     }
 
-    // Update the currentIndex to the next index.
+    /* Update the currentIndex to the next index. */
     if (argumentSize == 0) {
-        *currentIndex = nextIndex - 1;  // No size modifier found, so revert the currentIndex.
+        *currentIndex = nextIndex - 1;  /* No size modifier found, so revert the currentIndex. */
     } else {
-        *currentIndex = nextIndex;  // Update currentIndex to the next index.
+        *currentIndex = nextIndex;  /* Update currentIndex to the next index. */
     }
 
-    return argumentSize;  // Return the determined argument size.
+    return argumentSize;  /* Return the determined argument size. */
 }
