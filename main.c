912e585106201a197f2a46efadae8f12ff86baeb
EL HAKIK Amina <amina@gmail.com>
2023-08-22 14:49:49 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+    int num = 42;
+    char *str = "Hello, World!";
+
+    _printf("This is a number: %d\n", num);
+    _printf("This is a string: %s\n", str);
+
+    return (0);
+}
+
