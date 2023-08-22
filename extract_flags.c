6728a08849eb473b053bad641fc2f2b33a6e32be
AsmaAoub <aaoubraim@gmail.com>
2023-08-22 05:18:46 -0700

    a

---
+++ extract_flags.c
@@ -0,0 +1,43 @@
+#include "main_of_printf.h"
+
+/**
+ * getFlags - Extracts and calculates active flags from the format string.
+ * @format: The formatted string.
+ * @currentIndex: Pointer to the current index within the format string.
+ * 
+ * Return: Calculated flags.
+ */
+int extract_flags(const char *format, int *currentIndex) {
+    // Flag characters and their corresponding values
+    const char FLAG_CHARS[] = {'-', '+', '0', '#', ' ', '\0'};
+    const int FLAG_VALUES[] = {FLAG_MINUS, FLAG_PLUS, FLAG_ZERO, FLAG_HASH, FLAG_SPACE, 0};
+
+    int flags = 0;
+    int currentIdx = *currentIndex + 1; // Start checking after the '%'
+
+    // Loop through the format string starting from the current index
+    while (format[currentIdx] != '\0') {
+        int flagIndex;
+
+        // Check if the character is one of the flag characters
+        for (flagIndex = 0; FLAG_CHARS[flagIndex] != '\0'; flagIndex++) {
+            if (format[currentIdx] == FLAG_CHARS[flagIndex]) {
+                // Set the corresponding flag using bitwise OR (|)
+                flags |= FLAG_VALUES[flagIndex];
+                break;
+            }
+        }
+
+        // If it's not a flag character, stop searching
+        if (FLAG_CHARS[flagIndex] == '\0') {
+            break;
+        }
+
+        currentIdx++; // Move to the next character in the format string
+    }
+
+    // Update the current index to point to the last processed character
+    *currentIndex = currentIdx - 1;
+
+    return flags; // Return the calculated flags
+}
---
+++ extract_size.c
@@ -0,0 +1,32 @@
+#include "main_of_printf.h"
+
+/**
+ * extract_size - Determines the size of the argument data type.
+ * @formatSpecifier: Format specifier string.
+ * @currentIndex: Current index within the formatSpecifier string.
+ *
+ * Return: Size of the argument (e.g., S_LONG for 'l', S_SHORT for 'h', or 0 if no size modifier).
+ */
+int extract_size(const char *formatSpecifier, int *currentIndex)
+{
+    int nextIndex = *currentIndex + 1;  // Move to the next character in the formatSpecifier string.
+    int argumentSize = 0;  // Initialize the argument size to zero.
+
+    // Check if the next character in the formatSpecifier is 'l' (long).
+    if (formatSpecifier[nextIndex] == 'l') {
+        argumentSize = SIZE_LONG;  // Set the argument size to SIZE_LONG.
+    }
+    // Check if the next character in the formatSpecifier is 'h' (short).
+    else if (formatSpecifier[nextIndex] == 'h') {
+        argumentSize = SIZE_SHORT;  // Set the argument size to SIZE_SHORT.
+    }
+
+    // Update the currentIndex to the next index.
+    if (argumentSize == 0) {
+        *currentIndex = nextIndex - 1;  // No size modifier found, so revert the currentIndex.
+    } else {
+        *currentIndex = nextIndex;  // Update currentIndex to the next index.
+    }
+
+    return argumentSize;  // Return the determined argument size.
+}
